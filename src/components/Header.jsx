f66f7b14f775f2806d9921ed6cf593a00f31af85
mokamalelidn <mhmdalknj496@gmail.com>
2024-07-26 10:41:34 +0300

    chore: Update Header component to use relative image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
-import Logo from '../../public/investment-calculator-logo.png'
-
 export default function Header() {
     return (
         <header id="header">
-            <img src={ Logo } alt="Investment Calculator Logo" />
+            <img src={ "investment-calculator-logo.png" } alt="Investment Calculator Logo" />
             <h1 >Investment Calculator</h1>
         </header>
     )
